ed25d6f5b76ccf42bd714389d8acf24064363f55
Melissa Anderson <melissa.anderson@interapthq.com>
2016-10-30 14:15:56 -0400

    Got the search bar to show and stay in place, it does not search.

--- ToDoAppAnderson/ToDoTableViewController.swift
+++ ToDoAppAnderson/ToDoTableViewController.swift
@@ -9,13 +9,11 @@
 import UIKit
 import UserNotifications
 
-class ToDosTableViewController: UITableViewController {
+class ToDosTableViewController: UITableViewController{
     
     var onlyIfComplete = false
-    var searchDisplayController: UISearchController
-    let searchController = UISearchController(searchResultsController: nil)
-    
-    
+    var searchController = UISearchController(searchResultsController: nil)
+    var filteredToDo: [Category] = []
     
     @IBAction func showOnlyComplete(_ sender: AnyObject) {
         onlyIfComplete = !onlyIfComplete
@@ -25,28 +23,26 @@ class ToDosTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //      navigationItem.Toolbar = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
-        //       navigationItem.Toolbar = UIBarButtonI tem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
-        
         
         self.navigationItem.leftBarButtonItem = self.editButtonItem
+    
+        //      navigationItem.Toolbar = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        //       navigationItem.Toolbar = UIBarButtonI tem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+
         
-        searchController.searchResultsUpdater = self
-        searchController.dimsBackgroundDuringPresentation = false
-        searchController.searchBar.sizeToFit()
-        searchController.searchBar.delegate = self
-        searchController.searchBar.placeholder = "Search"
-        definesPresentationContext = true
-        tableView.tableHeaderView = searchController.searchBar
+                searchController.dimsBackgroundDuringPresentation = false
+                searchController.searchBar.sizeToFit()
+                searchController.searchBar.delegate = self
+                searchController.searchBar.placeholder = "Search"
+                definesPresentationContext = true
+                tableView.tableHeaderView = searchController.searchBar
     }
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Table view data source
+// MARK: - Table view data source
     // example contacts each section.. a, b, c.. the person name is a row.   we want a row for each toDo
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -55,12 +51,13 @@ class ToDosTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return ToDoStore.shared.getCount(category: section)
-    }
+        
+    return ToDoStore.shared.getCount(category: section)
+}
+  
     
-    // Mark: - switch for the section on the table view.
-    
+// Mark: - switch for the section on the table view.
+
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section{
         case 0:
@@ -73,176 +70,259 @@ class ToDosTableViewController: UITableViewController {
             return ""
         default:
             return "Section does not exist"
-        }
     }
+}
+
+override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ToDoTableViewCell.self)) as! ToDoTableViewCell
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ToDoTableViewCell.self)) as! ToDoTableViewCell
+    cell.setupCell(ToDoStore.shared.getToDo(indexPath.row, category: indexPath.section))
+    cell.setupCell(ToDoStore.shared.getToDo(indexPath.row, category: indexPath.section))
+    
+       return cell
+    
+    
+//MARK:  switch for complete:
+    
+    if onlyIfComplete == false {
         
-        cell.setupCell(ToDoStore.shared.getToDo(indexPath.row, category: indexPath.section))
-        cell.setupCell(ToDoStore.shared.getToDo(indexPath.row, category: indexPath.section))
-        
-        
-        //MARK:  switch for complete:
-        
-        if onlyIfComplete == false {
-            
-            if cell.toDo.completion == true {
-                cell.isHidden = true
-            }
-            
+        if cell.toDo.completion == true {
+            cell.isHidden = true
         }
-        return cell
     }
-    // takes empty space away when row is hidden
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return cell
+}
+
+// takes empty space away when row is hidden
+    override func tableView(_ tableView: UITableView, heightForRowAt    indexPath: IndexPath) -> CGFloat {
         if ToDoStore.shared.getToDo(indexPath.row, category: indexPath.section).completion == true && onlyIfComplete == false {
             return 0
         } else {
-            return 138
-        }
+            return 120
     }
-    
-    
-    
-    //MARK - SEARCH CONTROLLER SEARCH BAR
-    
-    //    let category: Category
-    //    if searchController.isActive && searchController.searchBar.text != "" {
-    //    category = filteredCategory[indexPath.row]
-    //    } else {
-    //    Category = category[indexPath.row]
-    //    }
-    //    cell.textLabel?.category: indexPath.section = indexPath.section
-    //    cell.detailTextLabel?.text = indexPath.section
-    
-    // Configure the cell...
-    
-    
-    //MARK: - ADDING THE ABILITY TO MAKE CELLS REORDERABLE
+}
+
+
+
+//MARK: - ADDING THE ABILITY TO MAKE CELLS REORDERABLE
     override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
-        return .delete
-    }
-    
+    return .delete
+}
+
     override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-    
-    
-    // Override to support conditional editing of the table view.
+    return false
+}
+
+
+// Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    
-    
-    
-    // Override to support editing the table view.
+    // Return false if you do not want the specified item to be editable.
+    return true
+}
+
+
+
+// Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            ToDoStore.shared.deleteToDo(indexPath.row, category: indexPath.section)  //  deletes rows in toDos as needed !!! AWESOME
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }
+    if editingStyle == .delete {
+        // Delete the row from the data source
+        ToDoStore.shared.deleteToDo(indexPath.row, category: indexPath.section)  //  deletes rows in toDos as needed !!! AWESOME
+        tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }
-    
-    // MARK: - Rearrange the table view.  use first.
-    
+}
+
+// MARK: - Rearrange the table view.  use first.
+
     override func tableView(_ tableView: UITableView, targetIndexPathForMoveFromRowAt sourceIndexPath: IndexPath, toProposedIndexPath proposedDestinationIndexPath: IndexPath) -> IndexPath {
-        if sourceIndexPath.section != proposedDestinationIndexPath.section {
-            return sourceIndexPath
-        } else {
-            return proposedDestinationIndexPath
-        }
+    if sourceIndexPath.section != proposedDestinationIndexPath.section {
+        return sourceIndexPath
+    } else {
+        return proposedDestinationIndexPath
     }
-    
-    //  Override to support rearranging the table view via priority.  use next
+}
+
+//  Override to support rearranging the table view via priority.  use next
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-        
-    }
     
-    // Override to support conditional rearranging of the table view.
+}
+
+// Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    
-    
-    //MARK: - LOCAL NOTIFICATION FUNCTION
+    // Return false if you do not want the item to be re-orderable.
+    return true
+}
+
+
+//MARK: - LOCAL NOTIFICATION FUNCTION
     func scheduleLocal() {
         let center = UNUserNotificationCenter.current()
-        
+    
         let content = UNMutableNotificationContent()
         content.title = "Wake UP"
         content.body = "Seize The Day."
         content.categoryIdentifier = "alarm"
         content.userInfo = ["customData": "fizzbuzz"]
         content.sound = UNNotificationSound.default()
-        
+    
         var dateComponents = DateComponents()
         dateComponents.hour = 10
         dateComponents.minute = 30
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
-        
+    
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
         center.add(request)
-    }
-    
-    
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    
-    /// this lets us connect the seque so that we can edit
+}
+
+
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+
+/// this lets us connect the seque so that we can edit
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "EditToDoSegue" {
             let toDoDetailVC = segue.destination as! ToDoDetailViewController
             let tableCell = sender as! ToDoTableViewCell
             toDoDetailVC.toDo = tableCell.toDo
-        }
+    }
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
+}
     
-    //MARK: EDIT BAR BUTTON
-    @IBAction func showEditing(sender: UIBarButtonItem)
+    
+//MARK: EDIT BAR BUTTON
+@IBAction func showEditing(sender: UIBarButtonItem)
+{
+    if(self.tableView.isEditing == true)
     {
-        if(self.tableView.isEditing == true)
-        {
-            self.tableView.isEditing = false
-            self.navigationItem.rightBarButtonItem?.title = "Done"
-        }
-        else
-        {
-            self.tableView.isEditing = true
-            self.navigationItem.rightBarButtonItem?.title = "Edit"
-        }
+        self.tableView.isEditing = false
+        self.navigationItem.rightBarButtonItem?.title = "Done"
     }
-    
-    //MARK: unwind Segue   ---  this lets us edit the toDos// else adds another toDo so user can add as many toDos  as possible
-    
-    @IBAction func saveToDoDetail(_ segue: UIStoryboardSegue){
-        let toDoDetailVC = segue.source as! ToDoDetailViewController
-        if let indexPath = tableView.indexPathForSelectedRow{
-            ToDoStore.shared.sort()
+    else
+    {
+        self.tableView.isEditing = true
+        self.navigationItem.rightBarButtonItem?.title = "Edit"
+    }
+}
+
+//MARK: unwind Segue   ---  this lets us edit the toDos// else adds another toDo so user can add as many toDos  as possible
+
+@IBAction func saveToDoDetail(_ segue: UIStoryboardSegue){
+    let toDoDetailVC = segue.source as! ToDoDetailViewController
+    if let indexPath = tableView.indexPathForSelectedRow{
+        ToDoStore.shared.sort()
+        
+        var indexPaths: [IndexPath] = []
+        for index in 0...indexPath.row {
+            indexPaths.append(IndexPath(row: index, section: 0))
             
-            var indexPaths: [IndexPath] = []
-            for index in 0...indexPath.row {
-                indexPaths.append(IndexPath(row: index, section: 0))
-                
-            }
-            
-            tableView.reloadRows(at: indexPaths, with: .automatic)
-        }else{
-            toDoDetailVC.toDo.priority = ToDoStore.shared.getPriority(category: toDoDetailVC.toDo.category)
-            ToDoStore.shared.addToDo(toDoDetailVC.toDo, category: toDoDetailVC.toDo.category)
-            let indexPath = IndexPath(row: 0, section: toDoDetailVC.toDo.category)
-            tableView.insertRows(at: [indexPath], with: .automatic)// several options available automatic - when upgrades are made the style will be changed to defaults to other systems.
         }
         
+        tableView.reloadRows(at: indexPaths, with: .automatic)
+    }else{
+        toDoDetailVC.toDo.priority = ToDoStore.shared.getPriority(category: toDoDetailVC.toDo.category)
+        ToDoStore.shared.addToDo(toDoDetailVC.toDo, category: toDoDetailVC.toDo.category)
+        let indexPath = IndexPath(row: 0, section: toDoDetailVC.toDo.category)
+        tableView.insertRows(at: [indexPath], with: .automatic)// several options available automatic - when upgrades are made the style will be changed to defaults to other systems.
+        }
     }
-    
 }
+
+//  MARK: - UISearchBarDelegate
+extension ToDosTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchText = searchBar.text, searchText != "" {
+        }
+    }
+}
+
+
+// MARK - SEARCH BAR FILTER
+                    //This function is to search lowercase strings
+//    func filterContentForSearchText(searchText: String, scope: String = "All") {
+//        filteredToDo = filteredToDo.filter { Category in
+//            return        filteredToDo.Category.lowercaseString.containsString(searchText.lowercaseString)
+//        }
+//        tableView.reloadData()
+//    }
+//
+            //This function is to show that the search has been triggered
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if searchController.isactive && searchController.searchBar.text != "" {
+//            return filteredToDo.count
+//        }
+//        return filteredToDo.count
+//    }
+
+
+
+                //went in cell for row at
+//search bar
+//        let category: Category
+//        if searchController.searchBar.isactive && searchController.searchBar.text != "" {
+//            category = filteredToDo[indexPath.row]
+//        } else {
+//            category = filteredToDo[indexPath.row]
+//        }
+//        cell.textLabel?.text = filteredToDo.category.section
+//        cell.detailTextLabel?.text = filteredToDo.category.section
+
+                // tells the app to reload the information from search
+//    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+//        self.filteredToDo(searchString)
+//        return true
+
+
+// MARK: - Search Segue
+//
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        super.prepare(for: segue, sender: sender)
+//        if let ToDoDetailVC = segue.destinationViewController as? ToDoViewController
+//        {
+//            // gotta check if we're currently searching
+//            if self.searchDisplayController!.active {
+//                let indexPath = self.searchDisplayController?.searchResultsTableView.indexPathForSelectedRow()
+//                if indexPath != nil {
+//                    ToDoDetailVC.species = self.ToDoSearchResults?[indexPath!.row]
+//                }
+//            } else {
+//                let indexPath = self.tableview?.indexPathForSelectedRow()
+//                if indexPath != nil {
+//                    ToDoDetailVC.category = self.category?[indexPath!.row]
+//                }
+//            }
+//        }
+//    }
+//
+//
+
+//        self.filteredToDo = self.filteredToDo.filter({( filteredToDo: Category) -> Bool in
+//            var fieldToSearch: String?
+//            switch (scope) {
+//            case (0):
+//                fieldToSearch = toDo.title
+//            default:
+//                fieldToSearch = nil
+//            }
+//            if fieldToSearch == nil {
+//                self.ToDoSearchResults = nil
+//                return false
+//            }
+//            return fieldToSearch!.lowercaseString.rangeOfString(searchText.lowercaseString) != nil
+//        })
+//    }
+//
+//    func searchDisplayController(controller: UISearchDisplayController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+//        let selectedIndex = controller.searchBar.selectedScopeButtonIndex
+//        self.filterContentForSearchText(searchString, scope: selectedIndex)
+//        return true
+//    }
+//
+//    func searchDisplayController(controller: UISearchDisplayController, shouldReloadTableForSearchScope searchOption: Int) -> Bool {
+//        let searchString = controller.searchBar.text
+//        self.filterContentForSearchText(searchString, scope:searchOption)
+//        return true
+//    }
 
